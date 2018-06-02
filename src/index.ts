af3ea78f4b37e0a490851ad5383e76e75d75d04a
Yefimchuk Roman <yefimchuk.roman@gmail.com>
2018-06-02 18:02:48 +0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,35 @@
+export default function promiseLazyEach<T>(values: T[], promiseFactory: (value: T, index?: number) => Promise<any>): Promise<void> {
+
+    if (values.length === 0) {
+
+        return Promise.resolve();
+    }
+
+    return new Promise<void>((resolve: () => void, reject: (error: any) => void) => {
+
+        const nextIteration: (index?: number) => void = (index: number = 0): void => {
+
+            if (index === promiseFactory.length) {
+
+                resolve();
+            } else {
+
+                promiseFactory(values[index], index).then((result: any): void => {
+
+                    if (typeof result === 'boolean' && !result) {
+
+                        resolve();
+                        return;
+                    }
+
+                    nextIteration(index + 1);
+                }, (error: any): void => {
+
+                    reject(error);
+                });
+            }
+        };
+
+        nextIteration();
+    });
+}
